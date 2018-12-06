1c99837d42dff7e1ae7f2bcd4bad4cd6fa297b96
bluna5 <44988243+bluna5@users.noreply.github.com>
2018-12-05 18:56:41 -0700

    unfinished database

---
+++ accountDatabase/Models/Account.cs
@@ -0,0 +1,24 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+using MongoDB.Bson;
+using MongoDB.Bson.Serialization.Attributes;
+
+namespace accountDatabase.Models
+{
+    public class Book
+    {
+        public ObjectId Id { get; set; }
+        [BsonElement("BookId")]
+        public int BookId { get; set; }
+        [BsonElement("BookName")]
+        public string BookName { get; set; }
+        [BsonElement("Price")]
+        public int Price { get; set; }
+        [BsonElement("Category")]
+        public string Category { get; set; }
+        [BsonElement("Author")]
+        public string Author { get; set; }
+    }
+}
